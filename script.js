6f1b765545c952b345f9f2a0366a8864246db9a2
8Krystof8 <132814628+8Krystof8@users.noreply.github.com>
2024-10-23 10:08:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function scrollToSection(sectionId) {
 }
 
 // Funkce pro zobrazení tlačítka "Nahoru"
-window.onscroll = function() {
+window.addEventListener('scroll', function() {
     let backToTop = document.getElementById('backToTop');
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    if (window.pageYOffset > 500) {
         backToTop.style.display = "block";
     } else {
         backToTop.style.display = "none";
     }
-};
+});
 
 // Funkce pro plynulý posun nahoru
 function scrollToTop() {
@@ -30,10 +30,3 @@ window.onload = function() {
         bar.style.setProperty('--percent', bar.getAttribute('data-percent'));
     });
 };
-
-// Validace a odeslání formuláře
-document.querySelector('#contact form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Děkujeme za zprávu!');
-    this.reset();
-});
